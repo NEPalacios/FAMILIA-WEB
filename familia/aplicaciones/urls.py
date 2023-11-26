b12b434f30bb38c0afc233a4f9437c7372b9bdf2
NicolasPalacios <palaciosnicolas94@gmail.com>
2023-11-26 18:39:06 -0300

    docs: nuevas funciones. Servidor ON

--- familia/aplicaciones/urls.py
+++ familia/aplicaciones/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from django.shortcuts import render
+from django.http import HttpResponse
 
-def inicio_view(request):
-    return "BIENVENIDOS"
+def inicio_view(xx):
+    return HttpResponse("BIENVENIDOS, ESTA ES UNA PAGINA DE INICIO SOBRE MIS FAMILIARES: ")
+
+def padres_view(xx):
+    return HttpResponse("ELLOS SON MIS PADRES: ")
+
+def hermanos_view(xx):
+    return HttpResponse("ELLOS SON MIS HERMANOS: ")
+
+
+
+
+
 
 urlpatterns = [
-    path("inicio/", inicio_view)    
+    path("inicio/", inicio_view),    
+    path("padres/", padres_view),
+    path("hermanos/", hermanos_view),
 ]
